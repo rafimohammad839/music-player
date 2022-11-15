b4779b8c57326e685551375736781a5c4ee294ec
Mohammad Rafi <rafi.mohammad@autodesk.com>
2022-11-15 20:42:34 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -1,43 +1,55 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import Player from "./components/Player";
 
 function App() {
-  const [songs, setSongs] = useState([
+  const [songs] = useState([
     
     {
-      title: "Forget me too ft. Halsey",
-      artist: "Machine Gun Kelly",
-      img_src: "./images/song-1.jpg",
-      src: "./music/on-n-on.mp3"
-    },
-    {
-      title: "Song 2",
-      artist: "Artist 2",
+      title: "On & On",
+      artist: "Carton",
       img_src: "./images/song-2.jpg",
-      src: "./music/somebody-new.mp3"
-    },
-    {
-      title: "Song 3",
-      artist: "Artist 3",
-      img_src: "./images/song-3.jpg",
       src: "./music/on-n-on.mp3"
     },
     {
-      title: "Song 4",
-      artist: "Artist 4",
+      title: "Waka Waka",
+      artist: "Shakira",
+      img_src: "./images/song-1.jpg",
+      src: "./music/Waka Waka - Shakira.mp3"
+    },
+    {
+      title: "I'm The One ft. Justin Bieber",
+      artist: "DJ Khaled",
+      img_src: "./images/song-3.jpg",
+      src: "./music/I'm the One(Mr-Jatt1.com).mp3"
+    },
+    {
+      title: "Despacito",
+      artist: "Justin Bieber",
       img_src: "./images/song-4.jpg",
-      src: "./music/somebody-new.mp3"
+      src: "./music/Despacito - Justin Bieber.mp3"
     }
   ])
 
   const [currentSongIndex, setCurrentSongIndex] = useState(0)
   const [nextSongIndex, setNextSongIndex] = useState(currentSongIndex + 1)
 
+  useEffect(() => {
+    setNextSongIndex(() => {
+      if (currentSongIndex + 1 > songs.length - 1) {
+        return 0;
+      } else {
+        return currentSongIndex + 1;
+      }
+    })
+  }, [currentSongIndex])
+
   return (
     <div className="App">
       <Player
-        song={songs[currentSongIndex]}
-        nextSong={songs[nextSongIndex]} 
+        currentSongIndex={currentSongIndex}
+        setCurrentSongIndex={setCurrentSongIndex}
+        nextSongIndex={nextSongIndex}
+        songs={songs}
       />
     </div>
   );
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,17 +1,63 @@
-import React from 'react'
-import PlayerControl from './PlayerControl'
-import PlayerDetails from './PlayerDetails'
+import React, { useState, useRef, useEffect } from "react";
+import PlayerControl from "./PlayerControl";
+import PlayerDetails from "./PlayerDetails";
 
 const Player = (props) => {
-  return (
-    <div className='c-player'>
-      <audio></audio>
-      <h4>Playing Now</h4>
-      <PlayerDetails song={props.song} />
-      <PlayerControl />
-      <p><strong>Next up:</strong> {props.nextSong.title} by {props.nextSong.artist}</p>
-    </div>
-  )
-}
+  const audioEl = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
 
-export default Player
+  useEffect(() => {
+    if (isPlaying) {
+      audioEl.current.play();
+    } else {
+      audioEl.current.pause();
+    }
+  });
+
+  const skipSong = (forwards = true) => {
+    if (forwards) {
+      props.setCurrentSongIndex(() => {
+        let temp = props.currentSongIndex;
+        temp++;
+
+        if (temp > props.songs.length - 1) {
+          temp = 0;
+        }
+
+        return temp;
+      });
+    } else {
+      props.setCurrentSongIndex(() => {
+        let temp = props.currentSongIndex;
+        temp--;
+
+        if (temp < 0) {
+          temp = props.songs.length - 1;
+        }
+
+        return temp;
+      });
+    }
+  };
+
+  return (
+    <div className="c-player">
+        <audio src={props.songs[props.currentSongIndex].src} ref={audioEl}></audio>
+        <h4>Playing Now</h4>
+        <PlayerDetails
+            song={props.songs[props.currentSongIndex]} 
+        />
+        <PlayerControl
+            isPlaying={isPlaying}
+            setIsPlaying={setIsPlaying}
+            skipSong={skipSong}
+        />
+        <p>
+            <strong>Next up:</strong> {props.songs[props.nextSongIndex].title} by{" "}
+            {props.songs[props.nextSongIndex].artist}
+        </p>
+    </div>
+  );
+};
+
+export default Player;
